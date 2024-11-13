99c1bd7ecce8035c2eb4ec16619f994a42c4ecde
Caetano <122699973+ProgCaetano@users.noreply.github.com>
2024-11-13 18:39:11 -0300

    editado

--- Pratica 6.1 - Conta exeption/src/empresa/Principal.java
+++ Pratica 6.1 - Conta exeption/src/empresa/Principal.java
@@ -4,24 +4,25 @@ public class Principal {
 
 	public static void main(String[] args) {
 		
-		Conta c1 = new Conta("Mario", 5000);
-		Conta c2 = new Conta("Luige", 2000);
+		Conta c1 = new Conta("Kratos", 6500);
+		Conta c2 = new Conta("Hades", 3000);
+		
 		
 		try {
 			c1.info();
-			c1.depositar(300);
+			c1.depositar(250);
 			c1.info();
-			c1.sacar(800);
+			c1.sacar(67);
 			c1.info();
 			c2.info();
 			System.out.println("-----------------------------");
 			}
 		catch(Exception e) {
-			System.out.println("Ocorreu um erro, coloque outro valor");
+			System.out.println("erro, coloque outro valor");
 			System.out.println(e.getMessage());
 			}
 		finally {
-			System.out.println("Fechando o problema");
+			System.out.println("Fechando ..");
 		}
 		
 		System.out.println("Transgerindo 400...");
