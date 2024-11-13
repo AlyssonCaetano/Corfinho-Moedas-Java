0b679a969c903cf7f619dce5a3d8cda903bf455c
Caetano <122699973+ProgCaetano@users.noreply.github.com>
2024-11-13 16:56:08 -0300

    teste

--- Pratica 6.1 - Conta exeption/src/empresa/Principal.java
+++ Pratica 6.1 - Conta exeption/src/empresa/Principal.java
@@ -3,8 +3,15 @@ package empresa;
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Conta c1 = new Conta("Mario", 5000);
+		Conta c2 = new Conta("Luige", 2000);
+		
+		c1.info();
+		c1.depositar(300);
+		c1.info();
+		c1.sacar(800);
+		c1.info();
 	}
 
 }
